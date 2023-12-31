f7125c32f2f90892bfa2aa2ab434b90b337d2cd1
Kind2232 <151034253+kind5545@users.noreply.github.com>
2023-12-31 10:05:47 -0500

    Update chimney.py

--- chimney.py
+++ chimney.py
@@ -9,7 +9,13 @@ cmds = ["help [command]: Lists all commands in this IDLE \n", "about: Gives info
 print("Welcome to the Chimney IDLE!")
 print("Type \"help\" for a complete list of commands.")
 while True:
-	input = input("chimney> ")
+	input = input("> ")
 	if input == "help":
 		for i in range(len(cmds)):
 			print(cmds[x])
+	elif input == "about":
+		print("Chimney is a programming language that was made by \n Kind2232 (@kind5545 on github). He made this mainly because he was bored, \n but also to show people how programming actually works.")
+	elif "say" in input:
+		print(string[3:len(input)])
+	else:
+		print("Hmm... That isn't a command that exists.")
